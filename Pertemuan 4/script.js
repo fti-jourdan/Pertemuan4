367457e3a3340ed73fb57f8face828f9f9641501
fti-jourdan <70849808+fti-jourdan@users.noreply.github.com>
2020-09-23 12:24:41 +0700

    Add files via upload

---
+++ Pertemuan 4/script.js
@@ -0,0 +1,7 @@
+function changeLine1Style(){
+    document.getElementById("cLine1").className ="Makered";
+}
+
+function ChangeStyle(id, classname){
+    document.getElementById(id).className =classname;
+}
